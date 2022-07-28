fcff3833ed14238dda693aa3c6068933e24a23e1
Moh.Nur Ardiansya <102220969+MohNurArdiansya@users.noreply.github.com>
2022-07-28 11:44:16 +0800

    Update Tugas UAS.py

--- Tugas UAS.py
+++ Tugas UAS.py
@@ -1,12 +1,52 @@
-import numpy as np
-import cv2
+clc;
+clear;
+close all;
+warning off all;
 
-img = cv2.imread('capture.jpg')
+Img = imread('capture.jpg');
+figure, imshow(Img);
 
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+I = double(rgb2gray(Img));
+dlx = [-1 0 1];
+dly = [-1;0;1];
+Ix = conv2(I,dlx,'same');
+Iy = conv2(I,dly,'same');
+J = sqrt((Ix.^2)+(Iy.^2));
+figure, imshow(J,[]);
 
-cv2.imshow('gambar 1', img)
-cv2.imshow('gambar 2', gray)
+K = J>10;
+L = imclearborder(K);
+M = imfill(L,'holes');
+N = bwareaopen(M,100);
+figure, imshow(N);
+area = sum(sum(N));
 
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+O = bwmorph(N,'remove');
+figure, imshow(O);
+perimeter = sum(sum(O));
+
+[B,L] = bwboundaries(N,'noholes');
+figure,imshow(Img)
+hold on
+for k = 1:length(B)
+    boundary = B{k};
+    plot(boundary(:,2), boundary(:,1), 'y', 'linewidth', 2)
+end
+
+[row,col] = find(N==1);
+[a,b] = size(N);
+mask = false(a,b);
+mask(min(row):max(row),min(col):max(col))= 1;
+mask = bwperin(mask,8);
+mask = imdilate(mask,strel('square',4));
+R = Img(:,:,1);
+G = Img(:,:,2);
+B = Img(:,:,3);
+R(mask) = 255;
+G(mask) = 255;
+B(mask) = 0;
+RGB = cat(3,R,G,B);
+figure, imshow(RGB);
+
+crop = imcrop(Img,[min(col) min(row) max(col)-min(col) max(row)-min(row)]);
+figure, imshow(crop);
